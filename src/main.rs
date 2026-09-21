f30a7c8ea93c55d7cf3e34e2db903277779620b9
Martin Krämer <MartinKraemer84@gmail.com>
2026-09-21 17:44:49 +0200

    Disabled cmd window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, AtomicIsize, AtomicUsize, Ordering};
